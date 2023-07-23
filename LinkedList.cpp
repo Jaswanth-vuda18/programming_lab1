ad8c16a1d1e0520b0dac1e8a0f627b2ddb17e30b
Jaswanth-vuda18 <vuda_j@cs.iitr.ac.in>
2023-07-23 16:41:04 +0530

    singlyLL - added insert function

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -14,12 +14,57 @@ public:
 		this->data = data;
 		this->next = NULL;
 	}
+	Node<T>* getNext(){
+		return this->next;		
+	}
+	void setNext(Node<T>* node){
+		this->next = node;
+	}
+	T getData(){
+		return this->data;
+	}
 };
 
 template <typename T> class LinkedList{
-	
+private:
+	Node<T> *head;
+
+public:
+	LinkedList(){
+		this->head = NULL;
+	}
+
+	void insertNode(T data){
+		Node<T> *cur = head;
+		Node<T> *temp = new Node(data);
+		if(head == NULL){
+			head = temp;
+			return;	
+		}
+		while(cur->getNext() != NULL){
+			cur = cur->getNext();
+		}
+		cur->setNext(temp);
+	}	
+
+	void print(){
+		Node<T> *cur = head;
+		while(cur != NULL){
+			cout<<cur->getData()<<" ";
+			cur = (cur->getNext());
+		}
+		cout<<endl;
+	}
 };
 
 int main(){
-	cout<<"Hello"<<endl;
+	
+	LinkedList<int> list;
+	
+	list.insertNode(1);
+	list.insertNode(8);
+	list.insertNode(0);
+	list.insertNode(8);
+	
+	list.print();
 }
