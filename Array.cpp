784a43306d90e14aea8609e580d627376b362685
Jaswanth-vuda18 <vuda_j@cs.iitr.ac.in>
2023-07-30 06:12:21 +0530

    delete method in Array is added

--- Array.cpp
+++ Array.cpp
@@ -18,6 +18,10 @@ public:
 		}
 		cout << endl;
 	}
+	
+	int getSize(){
+		return size;
+	}
 
 	void addElement(int index, T data){
 		if(index >= size || index < 0){
@@ -27,8 +31,23 @@ public:
 		arr[index] = data;
 	}
 	
-	void deleteElement(int index){
-
+	void deleteElement(T data){
+		int flag = 0;
+		for(int i = 0;i < size;i++){
+			if(arr[i] == data){
+				int ind = i+1;
+				while(ind < size){
+					arr[i] = arr[ind];
+					ind++;
+					i++;
+				}
+				size--;
+				flag = 1;
+			}	
+		}
+		if(flag == 0){
+			cout << "There is no such element in the array!" << endl;
+		}
 	}
 	
 	bool search(T data){
@@ -42,15 +61,22 @@ public:
 };
 
 int main(){
-	Array<float> a(2);
+	Array<float> a(3);
 	a.addElement(0, 1.23);
 	a.addElement(1, 3.54);
 	a.addElement(2, 4.5);
 	
+	/*
 	if(a.search(3))
 		cout << "present" << endl;
 	else
 		cout << "Not present" << endl;
+	*/
 
 	a.getArray();
+	cout << "Size of the Array: " << a.getSize() << endl;
+
+	a.deleteElement(1.23);
+	a.getArray();
+	cout << "Size of the Array: " << a.getSize() << endl;
 }
